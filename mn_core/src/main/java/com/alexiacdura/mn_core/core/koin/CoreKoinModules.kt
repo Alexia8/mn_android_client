6c23fc7d97fa8491c963e3bcc03ac9eb5cf70cb5
Unknown <alexia.c.dura@me.com>
2019-06-13 02:21:53 +0200

    Koin updates without scoping
    Removed scopes

--- mn_core/src/main/java/com/alexiacdura/mn_core/core/koin/CoreKoinModules.kt
+++ mn_core/src/main/java/com/alexiacdura/mn_core/core/koin/CoreKoinModules.kt
@@ -2,6 +2,8 @@ package com.alexiacdura.mn_core.core.koin
 
 import com.alexiacdura.mn_core.core.factory.DefaultServiceFactory
 import com.alexiacdura.mn_core.core.factory.ServiceFactory
+import com.alexiacdura.mn_core.data.feed.FeedInteractor
+import com.alexiacdura.mn_core.data.feed.FeedInteractorImpl
 import com.alexiacdura.mn_core.data.network.star.StarApi
 import com.alexiacdura.mn_core.data.network.star.StarApiImpl
 import com.alexiacdura.mn_core.data.network.star.StarApiService
@@ -42,17 +44,24 @@ val coreKoinModules = module {
         ) as ServiceFactory<VoteApiService>
     }
 
-    single { UserApiImpl(serviceFactory = get(KoinNames.USER_API_SERVICE)) as UserApi }
+    single(KoinNames.USER_API) { UserApiImpl(serviceFactory = get(KoinNames.USER_API_SERVICE)) as UserApi }
     single { StarApiImpl(serviceFactory = get(KoinNames.STAR_API_SERVICE)) as StarApi }
     single { VoteApiImpl(serviceFactory = get(KoinNames.VOTE_API_SERVICE)) as VoteApi }
 
-    single { UserInteractorImpl(userApi = get()) as UserInteractor }
-    single { UserFeedInteractorImpl(userApi = get()) as UserFeedInteractor }
-    single { UserDataInteractorImpl(userApi = get()) as UserDataInteractor }
+    single { UserInteractorImpl(userApi = get(KoinNames.USER_API)) as UserInteractor }
+    single (KoinNames.USER_FEED) { UserFeedInteractorImpl(userApi = get(KoinNames.USER_API)) as UserFeedInteractor }
+    single (KoinNames.USER_DATA) { UserDataInteractorImpl(userApi = get(KoinNames.USER_API)) as UserDataInteractor }
 
     single { CreateStarInteractorImpl(starApi = get()) as CreateStarInteractor }
     single { DeleteStarInteractorImpl(starApi = get()) as DeleteStarInteractor }
 
     single { CreateVoteInteractorImpl(voteApi = get()) as CreateVoteInteractor }
     single { DeleteVoteInteractorImpl(voteApi = get()) as DeleteVoteInteractor }
+
+    single(KoinNames.FEED) {
+        FeedInteractorImpl(
+            userFeedInteractor = get(KoinNames.USER_FEED),
+            userDataInteractor = get(KoinNames.USER_DATA)
+        ) as FeedInteractor
+    }
 }
--- mn_core/src/main/java/com/alexiacdura/mn_core/core/koin/KoinNames.kt
+++ mn_core/src/main/java/com/alexiacdura/mn_core/core/koin/KoinNames.kt
@@ -6,6 +6,10 @@ object KoinNames {
     val MUSICNERDS_BASE_URL = named("MusicnerdsBaseUrl")
     val MUSICNERDS_IS_DEBUG = named("MusicnerdsIsDebug")
     val USER_API_SERVICE = named("FeedApiService")
+    val FEED = named("FeedInteractor")
+    val USER_API = named("UserApi")
+    val USER_FEED = named("UserFeedInteractor")
+    val USER_DATA = named("UserDataInteractor")
     val STAR_API_SERVICE = named("StarApiService")
     val VOTE_API_SERVICE = named("VoteApiService")
 }
